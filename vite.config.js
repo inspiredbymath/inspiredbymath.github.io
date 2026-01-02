49456de386fbd2b45e5f7ba173d59d40acfbcd0b
veeman05 <vinod@vmconsulting.org>
2026-01-02 13:24:21 -0500

    updated to deploy properly to github pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/mathfun/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        montyHall: resolve(__dirname, 'monty-hall.html'),
+        prisonersDilemma: resolve(__dirname, 'prisoners-dilemma.html'),
+        staircase: resolve(__dirname, 'staircase.html'),
+      },
+    },
+  },
 })
