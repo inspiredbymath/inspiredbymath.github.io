f8b3f77114bff66d177bcd631ef39a140a7a301a
veeman05 <vinod@vmconsulting.org>
2026-01-04 15:39:54 -0500

    Updates

--- _app/immutable/chunks/BRqp26Ta.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1uv79g3?.base??"/mathfun",a=globalThis.__sveltekit_1uv79g3?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/chunks/BVPHiKgd.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_11vb7ui?.base??"",a=globalThis.__sveltekit_11vb7ui?.assets??s??"";export{a,s as b};
